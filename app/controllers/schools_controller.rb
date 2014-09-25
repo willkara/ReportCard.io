049430ddb27a96c05d47d0e1f2950ccba3bd04bc
William Karavites <wkaravites@gmail.com>
2014-09-25 13:47:55 -0400

    Skeleton implementation for Schools

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,8 +1,17 @@
 class SchoolsController < ApplicationController
   def new
+    @school = School.new
   end
 
   def create
+    @school = School.new(school_params)
+
+
+    if @school.save
+      redirect_to @school
+    else
+      render 'new'
+    end
   end
 
   def index
@@ -10,8 +19,14 @@ class SchoolsController < ApplicationController
   end
 
   def show
+    @school = School.find(params[:id])
   end
 
   def update
+  end
+
+  private
+  def school_params
+    params.require(:school).permit(:name, :summary, :website, :address, :phone)
   end
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,7 @@
 class School < ActiveRecord::Base
+  validates :name, :summary, :website, :address, presence: true
+  has_many :departments
+  has_many :ru_classes
+  has_many :ru_professors
+
 end
